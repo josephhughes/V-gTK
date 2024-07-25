37c507161896abfdf2005ed1f1b5e88d158e7bef
sandeepkasaragod <sandeep.kolya@gmail.com>
2024-07-25 11:54:04 +0100

    retmax is counted based on the query and is removed from the argument

--- genBank_downloader.py
+++ genBank_downloader.py
@@ -8,7 +8,8 @@ from argparse import ArgumentParser
 from os.path import join
 
 # Fetch accession ID's
-def fetch_ids(search_term, base_url, retmax, email):
+def fetch_ids(search_term, base_url, email):
+	retmax = get_record_count(search_term, email)
 	search_url = base_url + "esearch.fcgi?db=nucleotide&term=txid" + search_term + "[Organism:exp]&retmax=" + str(retmax) + "&usehistory=y&email=" + email + "&retmode=json"
 	print (search_url)
 	response = requests.get(search_url)
@@ -16,6 +17,16 @@ def fetch_ids(search_term, base_url, retmax, email):
 	data = response.json()
 	ids = data["esearchresult"]["idlist"]
 	return ids
+
+def get_record_count(search_term, email):
+	base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+	search_url = f"{base_url}esearch.fcgi?db=nucleotide&term=txid{search_term}[Organism:exp]&retmode=json&email={email}"
+	
+	response = requests.get(search_url)
+	data = response.json()
+	
+	count = int(data['esearchresult']['count'])
+	return count
 
 # Fetch GenBank XML data in batches
 def fetch_genbank_data(ids, batch_size, base_url, email, output_dir):
@@ -28,7 +39,7 @@ def fetch_genbank_data(ids, batch_size, base_url, email, output_dir):
 		response.raise_for_status()
 		save_data(response.text, batch_count, output_dir)
 		batch_count = batch_count + batch_size
-		sleep(1) 
+		sleep(2) 
 
 # Save XML to file as batch
 def save_data(data, batch_size, output_dir):
@@ -41,7 +52,7 @@ def save_data(data, batch_size, output_dir):
 
 # Download from scratch
 def download(args):
-	ids = fetch_ids(args.search_term, args.base_url, args.max_ret, args.email)
+	ids = fetch_ids(args.search_term, args.base_url, args.email)
 	print("Found " + str(len(ids)) + " IDs")
 	fetch_genbank_data(ids, args.batch_size, args.base_url,args.email, args.output_dir)
 
@@ -49,7 +60,7 @@ if __name__ == "__main__":
 	parser = ArgumentParser(description='Download and update GenBank XML files for a given species')
 	parser.add_argument('-s', '--search_term', help='Search term, generally a taxaid example: 11292', required=True)
 	parser.add_argument('-o', '--output_dir', help='Output directory where all the XML files are stored', default='GenBank')
-	parser.add_argument('-r', '--max_ret', help='Max number of records to be retrieved. Default is 100000', default=100000, type=int)
+	#parser.add_argument('-r', '--max_ret', help='Max number of records to be retrieved. Default is 100000', default=100000, type=int)
 	parser.add_argument('-b', '--batch_size', help='Max number of XML files to pull and merge in to a single file', default=100, type=int)
 	parser.add_argument('--update', action='store_true', help='Run the script in update mode to download only new sequences')
 	parser.add_argument('-u', '--base_url', help='base url to downlaod the XML files', default='https://eutils.ncbi.nlm.nih.gov/entrez/eutils/')
